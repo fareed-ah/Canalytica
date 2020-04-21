e5752a78b2f190cd8e37ae81a55765c18892e5ff
Fareed Ahmad <fareedahmad@cmail.carleton.ca>
2020-04-21 12:48:11 -0400

    Update server api

--- server/api/routes/index.js
+++ server/api/routes/index.js
@@ -2,6 +2,26 @@ const express = require('express')
 const app = express()
 const {spawn} = require('child_process');
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 // Function to convert an Uint8Array to a string
 var uint8arrayToString = function(data){
     return String.fromCharCode.apply(null, data);
@@ -10,7 +30,7 @@ var uint8arrayToString = function(data){
 //Root
 app.get('/', (req,res) =>{
     //get content for root home page
-    res.send("Canalytica home page")
+    res.send("{'appname':'Canalytica home page'}")
 })
 
 //Root
